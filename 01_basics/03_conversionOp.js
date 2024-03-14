750e6f662597b0ecd9a11ec9b45532ce9b6aa894
MoreManik <120258899+MoreManik@users.noreply.github.com>
2024-03-14 00:07:08 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,34 @@
+let score = "33"
+//let score = "33abc"
+//let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof score);
+//console.log(typeof valueInNumber); //if any value of score is defined which not convert in num is o/p is NaN
+//console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""
+let isLoggedIn = "somesh"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+ // "" => false
+ // "somesh" => true
+
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
