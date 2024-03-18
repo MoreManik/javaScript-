f226aec29c2c7fe65194d85ce7b98562c42292b3
MoreManik <120258899+MoreManik@users.noreply.github.com>
2024-03-18 07:26:17 +0000

    scopes

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,7 +4,7 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 for (let i = 0; i < array.length; i++) {
     const element = array[i];
@@ -12,6 +12,46 @@ for (let i = 0; i < array.length; i++) {
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one (){
+    const username = "hitesh"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    two()
+
+
+}
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh")
+
+    {
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// ========= interesting =========================
+
+
+addone(5) //this will work
+function addone(num){
+    return num + 1
+}
+
+addTwo(5) //this will not work
+const addTwo = function(num){
+    return num + 2
+}
